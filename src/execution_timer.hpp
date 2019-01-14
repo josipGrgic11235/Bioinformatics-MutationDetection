52d7be9c14f6b9afc386dd4e1096834559a521b9
Majstor Grga <grgicjosip310@gmail.com>
2019-01-14 17:48:41 +0100

    Execution timer, compile fix

--- src/execution_timer.hpp
+++ src/execution_timer.hpp
@@ -5,7 +5,11 @@
 
 class ExecutionTimer
 {
+#if defined(__APPLE__) && defined(__MACH__)
   std::chrono::time_point<std::chrono::steady_clock, std::chrono::nanoseconds> start_value;
+#else
+  std::chrono::time_point<std::chrono::system_clock> start_value;
+#endif
 
 public:
   void start();
